f6c9ac66e6b9e8fb36e427f08d8485b875c0ff3d
Francescodiodaa <91884945+Francescodiodaa@users.noreply.github.com>
2021-12-08 19:41:18 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+//
+// Created by Francesco Dioda on 08/12/21.
+//
+#include<iostream>
+#include "lib.h"
+using namespace std;
+void matrice(int dimensione) {
+    float array[dimensione];
+    for (int i = 0; i <= dimensione - 1; i++) {
+        cin >> array[i];
+
+    }
+    for (int i = 0; i <= dimensione - 1; i++) {
+        if (array[0] < array[i]) {
+            array[0] = array[i];
+        } else {
+            array[0] = array[0];
+        }
+    }
+    cout << array[0];
+}
+
 
